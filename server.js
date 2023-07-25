a897d08e301639cf601422e4b9ae8750ecbb3972
Sebyeol23 <lixxil2323@gmail.com>
2023-07-25 19:01:51 +0900

    Fix(index.html): msg JSON 수정

--- server.js
+++ server.js
@@ -128,8 +128,8 @@ function createRoom(ws) {
 
   function switchTurn(roomId) {
     const room = gameRooms[roomId];
-    
     if (room && room.player1 && room.player2) {
+        room.currentPlayer == room.currentPlayer == "Black" ? "White" : "Black";
       room.player1.send(JSON.stringify({ type: 'turn_switch', currentPlayer: room.currentPlayer }));
       room.player2.send(JSON.stringify({ type: 'turn_switch', currentPlayer: room.currentPlayer }));
     }
