06d3734f8cdd20110dd941b1efd69af8f8283ef6
Sebyeol23 <lixxil2323@gmail.com>
2023-07-25 18:25:28 +0900

    Fix(index.html): msg JSON 수정

--- server.js
+++ server.js
@@ -121,8 +121,8 @@ function createRoom(ws) {
     const room = gameRooms[roomId];
     if (room && room.player1 && room.player2) {
       room.gameStarted = true;
-      room.player1.send(JSON.stringify({ type: 'game_start', color: "Black", currentPlayer: room.currentPlayer }));
-      room.player2.send(JSON.stringify({ type: 'game_start', color: "White", currentPlayer: room.currentPlayer }));
+      room.player1.send(JSON.stringify({ type: 'game_start', roomId: roomId, color: "Black", currentPlayer: room.currentPlayer }));
+      room.player2.send(JSON.stringify({ type: 'game_start', roomdId: roomId, color: "White", currentPlayer: room.currentPlayer }));
     }
   }
 
